84e68fe46da24f17080d2823fa747e29b958d067
Jb05ds <johnbertdelossantos480@gmail.com>
2025-10-16 14:56:01 +0800

    Design-Testing

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+const navigationBar = document.querySelector('.more-info-container div');
+navigationBar.addEventListener('click', function() {
+    if(navigationBar.contains('more-info-container div'){
+        document.body.style.background = 'gray';
+    })
+})
